eadd0cb1b05cde336179be82bc4a763f1ec248b4
vuquochinh <chinhvu2002@gmail.com>
2023-04-23 16:02:24 +0700

    'RegisterUpdate'

--- Hola_Resort/Hola_Resort/Controllers/UserController.cs
+++ Hola_Resort/Hola_Resort/Controllers/UserController.cs
@@ -18,9 +18,40 @@ namespace Hola_Resort.Controllers
         // GET Register
         public ActionResult Register()
         {
-            return View();
+            return View(new ViewModel.VMregister());
         }
-            
+        // POST Register
+        [HttpPost]
+        public ActionResult Register(ViewModel.VMregister model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Kiểm tra xem tên đăng nhập đã được sử dụng hay chưa
+                var checkUsername = data.Customers.Where(x => x.Username == model.Username).FirstOrDefault();
+                if (checkUsername != null)
+                {
+                    ModelState.AddModelError("", "Tên đăng nhập đã được sử dụng. Vui lòng chọn tên đăng nhập khác.");
+                    return View(model);
+                }
+                // Tạo đối tượng Customer mới
+                Customer customer = new Customer();
+                customer.CustomerId = model.CustomerId;
+                customer.FullName = model.FullName;
+                customer.Email = model.Email;
+                customer.PhoneNumber = model.PhoneNumber;
+                customer.Address = model.Address;
+                customer.DayofBirt = model.DayofBirt;
+                customer.Gender = model.Gender;
+                customer.Username = model.Username;
+                customer.Password = model.Password;
+                // Thêm đối tượng Customer mới vào CSDL
+                data.Customers.InsertOnSubmit(customer);
+                data.SubmitChanges();
+                return RedirectToAction("Home", "Index");
+            }
+            return View(model);
+        }
+
 
 
     }
--- Hola_Resort/Hola_Resort/ViewModel/VMregister.cs
+++ Hola_Resort/Hola_Resort/ViewModel/VMregister.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations;
 
 
 namespace Hola_Resort.ViewModel
