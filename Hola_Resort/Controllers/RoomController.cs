1b0255193fae159fc0502b379c5b47e919bf4eee
Quốc Tri <85606061+qtri13302@users.noreply.github.com>
2023-05-06 23:08:42 +0700

    'Room'

---
+++ Hola_Resort/Controllers/RoomController.cs
@@ -0,0 +1,34 @@
+﻿using Hola_Resort.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hola_Resort.Controllers
+{
+    public class RoomController : Controller
+    {
+        // GET: Room
+        HolaDBDataContext data = new HolaDBDataContext();
+
+        public ActionResult Room(string id)
+        {
+            var rooms = data.Rooms.Where(r => r.RoomTypeId == id).ToList();
+            
+            return View(rooms);
+        }
+        public ActionResult Details(string id)
+        {
+            var product = data.Rooms.FirstOrDefault(p => p.RoomId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+    }
+}
---
+++ Hola_Resort/Controllers/RoomTypeController.cs
@@ -0,0 +1,19 @@
+﻿using Hola_Resort.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hola_Resort.Controllers
+{
+    public class RoomTypeController : Controller
+    {
+        // GET: RoomType
+        HolaDBDataContext data = new HolaDBDataContext();
+        public ActionResult Index()
+        {
+            return View(data.RoomTypes.ToList());
+        }
+    }
+}
